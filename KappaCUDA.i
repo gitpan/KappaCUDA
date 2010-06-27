f5e20e9361b33c1f326380033763a75ec15894ce
Brian H. Dunford-Shore <brian@psilambda.com>
2010-06-27 16:19:08 +0000

    Import of BRIAN/KappaCUDA-1.2.0 from CPAN.
    gitpan-cpan-distribution: KappaCUDA
    gitpan-cpan-version:      1.2.0
    gitpan-cpan-path:         BRIAN/psilambdallc/KappaCUDA-1.2.0.tar.gz
    gitpan-cpan-author:       BRIAN
    gitpan-cpan-maturity:     released

--- KappaCUDA.i
+++ KappaCUDA.i
@@ -1,14 +1,29 @@
 %module KappaCUDA
 %include "std_string.i"
+#ifndef SWIGPHP
 %include "carrays.i"
 %array_class(int, intArray);
+%array_class(long, longArray);
 %array_class(unsigned, unsignedArray);
 %array_class(float, floatArray);
 %array_class(double, doubleArray);
+#endif
 
+#ifdef SWIGPERL
 %perlcode %{
-$KappaCUDA::VERSION = '1.1.1';
+$KappaCUDA::VERSION = '1.2.0';
 %}
+#endif
+
+#ifdef SWIGPYTHON
+%ignore None;
+#endif
+
+#ifdef SWIGRUBY
+// These give compile errors
+%ignore kappa::Arguments::Type;
+%ignore kappa::Value::Type;
+#endif
 
 // Ignore non-exported (hidden) methods and variables
 %ignore kappa::Kappa::Cancel;
@@ -60,6 +75,7 @@ $KappaCUDA::VERSION = '1.1.1';
 
 KAPPA_DLL_EXPORT int *intptr_fromvoidptr(void *voidptr);
 KAPPA_DLL_EXPORT unsigned *unsignedptr_fromvoidptr(void *voidptr);
+KAPPA_DLL_EXPORT long *longptr_fromvoidptr(void *voidptr);
 KAPPA_DLL_EXPORT float *floatptr_fromvoidptr(void *voidptr);
 KAPPA_DLL_EXPORT double *doubleptr_fromvoidptr(void *voidptr);
 
@@ -89,6 +105,7 @@ using namespace kappa;
 
 KAPPA_DLL_EXPORT int *intptr_fromvoidptr(void *voidptr);
 KAPPA_DLL_EXPORT unsigned *unsignedptr_fromvoidptr(void *voidptr);
+KAPPA_DLL_EXPORT long *longptr_fromvoidptr(void *voidptr);
 KAPPA_DLL_EXPORT float *floatptr_fromvoidptr(void *voidptr);
 KAPPA_DLL_EXPORT double *doubleptr_fromvoidptr(void *voidptr);
 
@@ -101,6 +118,10 @@ KAPPA_DLL_EXPORT unsigned *unsignedptr_fromvoidptr(void *voidptr) {
     return (unsigned *)voidptr;
 }
 
+KAPPA_DLL_EXPORT long *longptr_fromvoidptr(void *voidptr) {
+    return (long *)voidptr;
+}
+
 KAPPA_DLL_EXPORT float *floatptr_fromvoidptr(void *voidptr) {
     return (float *)voidptr;
 }
@@ -109,3 +130,37 @@ KAPPA_DLL_EXPORT double *doubleptr_fromvoidptr(void *voidptr) {
     return (double *)voidptr;
 }
 %}
+
+#ifdef SWIGPYTHON
+KAPPA_DLL_EXPORT kappa::Command *kappaCommand_frompycobject(PyObject *voidptr);
+KAPPA_DLL_EXPORT kappa::Kappa *kappaKappa_frompycobject(PyObject *voidptr);
+KAPPA_DLL_EXPORT kappa::Process *kappaProcess_frompycobject(PyObject *voidptr);
+KAPPA_DLL_EXPORT kappa::Attributes *kappaAttributes_frompycobject(PyObject *voidptr);
+KAPPA_DLL_EXPORT kappa::Arguments *kappaArguments_frompycobject(PyObject *voidptr);
+KAPPA_DLL_EXPORT kappa::Resource *kappaResource_frompycobject(PyObject *voidptr);
+KAPPA_DLL_EXPORT kappa::ProcessControlBlock *kappaPCB_frompycobject(PyObject *voidptr);
+
+%{
+KAPPA_DLL_EXPORT kappa::Command *kappaCommand_frompycobject(PyObject *pycptr) {
+  return (kappa::Command *)PyCObject_AsVoidPtr (pycptr);
+}
+KAPPA_DLL_EXPORT kappa::Kappa *kappaKappa_frompycobject(PyObject *pycptr) {
+  return (kappa::Kappa *)PyCObject_AsVoidPtr (pycptr);
+}
+KAPPA_DLL_EXPORT kappa::Process *kappaProcess_frompycobject(PyObject *pycptr) {
+  return (kappa::Process *)PyCObject_AsVoidPtr (pycptr);
+}
+KAPPA_DLL_EXPORT kappa::Attributes *kappaAttributes_frompycobject(PyObject *pycptr) {
+  return (kappa::Attributes *)PyCObject_AsVoidPtr (pycptr);
+}
+KAPPA_DLL_EXPORT kappa::Arguments *kappaArguments_frompycobject(PyObject *pycptr) {
+  return (kappa::Arguments *)PyCObject_AsVoidPtr (pycptr);
+}
+KAPPA_DLL_EXPORT kappa::Resource *kappaResource_frompycobject(PyObject *pycptr) {
+  return (kappa::Resource *)PyCObject_AsVoidPtr (pycptr);
+}
+KAPPA_DLL_EXPORT kappa::ProcessControlBlock *kappaPCB_frompycobject(PyObject *pycptr) {
+  return (kappa::ProcessControlBlock *)PyCObject_AsVoidPtr (pycptr);
+}
+%}
+#endif
--- cuda/matrixMul_kernel.cu
+++ cuda/matrixMul_kernel.cu
@@ -1,36 +1,16 @@
 /*
- * Copyright 1993-2007 NVIDIA Corporation.  All rights reserved.
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
  *
- * NOTICE TO USER:
+ * NVIDIA Corporation and its licensors retain all intellectual property and 
+ * proprietary rights in and to this software and related documentation. 
+ * Any use, reproduction, disclosure, or distribution of this software 
+ * and related documentation without an express license agreement from
+ * NVIDIA Corporation is strictly prohibited.
  *
- * This source code is subject to NVIDIA ownership rights under U.S. and
- * international Copyright laws.  Users and possessors of this source code
- * are hereby granted a nonexclusive, royalty-free license to use this code
- * in individual and commercial software.
- *
- * NVIDIA MAKES NO REPRESENTATION ABOUT THE SUITABILITY OF THIS SOURCE
- * CODE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS" WITHOUT EXPRESS OR
- * IMPLIED WARRANTY OF ANY KIND.  NVIDIA DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOURCE CODE, INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL,
- * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS,  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
- * OR OTHER TORTIOUS ACTION,  ARISING OUT OF OR IN CONNECTION WITH THE USE
- * OR PERFORMANCE OF THIS SOURCE CODE.
- *
- * U.S. Government End Users.   This source code is a "commercial item" as
- * that term is defined at  48 C.F.R. 2.101 (OCT 1995), consisting  of
- * "commercial computer  software"  and "commercial computer software
- * documentation" as such terms are  used in 48 C.F.R. 12.212 (SEPT 1995)
- * and is provided to the U.S. Government only as a commercial end item.
- * Consistent with 48 C.F.R.12.212 and 48 C.F.R. 227.7202-1 through
- * 227.7202-4 (JUNE 1995), all U.S. Government End Users acquire the
- * source code with only those rights set forth herein.
- *
- * Any use of this source code in individual and commercial software must
- * include, in the user documentation and internal comments to the code,
- * the above Disclaimer and U.S. Government End Users Notice.
+ * Please refer to the applicable NVIDIA end user license agreement (EULA) 
+ * associated with this source code for terms and conditions that govern 
+ * your use of this NVIDIA software.
+ * 
  */
 
 /* Matrix multiplication: C = A * B.
@@ -51,17 +31,6 @@
 #define AS(i, j) As[i][j]
 #define BS(i, j) Bs[i][j]
 #endif
-
-__constant__ float constData[8192];
-
-// declare texture reference for 1D float texture
-texture<float, 1, cudaReadModeElementType> tex_1D;
-
-// declare texture reference for 2D float texture
-texture<float, 2, cudaReadModeElementType> tex_2D;
-
-// declare texture reference for 3D float texture
-texture<float, 3, cudaReadModeElementType> tex_3D;
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Matrix multiplication on the device: C = A * B
@@ -137,78 +106,6 @@ matrixMul( float* C, float* A, float* B, int wA, int wB)
     // each thread writes one element
     int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
     C[c + wB * ty + tx] = Csub;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Fetch from texture lookup
-//! @param g_odata output data in global memory
-////////////////////////////////////////////////////////////////////////////////
-extern "C"
-__global__ void
-fetchTexOneD( float* g_odata, int width) 
-{
-    // calculate normalized texture coordinates
-    unsigned int x = threadIdx.x;
-
-    // read from texture and write to global memory
-    g_odata[x] = tex1Dfetch(tex_1D, x);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Get from texture lookup
-//! @param g_odata output data in global memory
-////////////////////////////////////////////////////////////////////////////////
-extern "C"
-__global__ void
-getTexOneD( float* g_odata, int width) 
-{
-    // calculate normalized texture coordinates
-    unsigned int x = threadIdx.x;
-
-    float u = ((float)x + 0.5f)/ (float) width;
-
-    // read from texture and write to global memory
-    g_odata[x] = tex1D(tex_1D, u);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Get from texture lookup
-//! @param g_odata output data in global memory
-////////////////////////////////////////////////////////////////////////////////
-extern "C"
-__global__ void
-getTexTwoD( float* g_odata, int width, int height) 
-{
-    // calculate normalized texture coordinates
-    unsigned int x = threadIdx.x;
-    unsigned int y = threadIdx.y;
-
-    float u = ((float)x + 0.25f)/ (float) width;
-    float v = ((float)y + 0.25f)/ (float) height;
-
-    // read from texture and write to global memory
-    g_odata[y*width + x] = tex2D(tex_2D, u, v);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Get from texture lookup
-//! @param g_odata output data in global memory
-////////////////////////////////////////////////////////////////////////////////
-extern "C"
-__global__ void
-getTexThreeD( float* g_odata, int width, int height, int depth) 
-{
-    // calculate normalized texture coordinates
-    unsigned int x = threadIdx.x;
-    unsigned int y = threadIdx.y;
-    unsigned int z = threadIdx.z;
-
-    float u = ((float)x + 0.75f)/ (float) width;
-    float v = ((float)y + 0.75f)/ (float) height;
-    float w = ((float)z + 0.75f)/ (float) depth;
-
-    // read from texture and write to global memory
-    g_odata[(height*z + y)*width + x] = tex3D(tex_3D, u, v, w);
 }
 
 #endif // #ifndef _MATRIXMUL_KERNEL_H_
